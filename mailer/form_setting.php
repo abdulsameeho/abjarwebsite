dd260a724710700f309c30423f4aa31a8e04fb3c
abdulsameeh <abdulsameeho26@gmail.com>
2022-04-24 16:30:24 +0530

    finalcommit

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from Website"; //letter subject
+	$to = '';  info@abjarbranding.com
+	$from = 'info@abjarbranding.com'; // Admin e-mail
+	$fromName = 'Abjar Branding'; // Your company name
+	$charset = 'UTF-8';
+?>
